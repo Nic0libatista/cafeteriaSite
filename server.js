994763dab8fe17a005e2ce6fc35ad625bf257fba
nic0libatista <nicolisantosbatista@gmail.com>
2026-02-13 17:18:13 -0300

    teste nodemailer

--- server.js
+++ server.js
@@ -31,14 +31,11 @@ app.get('/api/produtos', async (req, res) => {
 
 const transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
-    port: 587,
-    secure: false,
+    port: 465,
+    secure: true, 
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
-    },
-    tls: {
-        rejectUnauthorized: false
     }
 });
 
